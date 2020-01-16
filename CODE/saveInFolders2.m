46726faea0d36d6ec5292cd433a6b5f1247e5c11
CCRA <ccr22@cm3-1-01-118.managed.city.ac.uk>
2020-01-16 09:54:50 +0000

    Update saveInFolders2.m

--- CODE/saveInFolders2.m
+++ CODE/saveInFolders2.m
@@ -1,16 +1,22 @@
-clear
-dir0 = dir('T*');
 
+clear
+
+baseDir     = 'SingleSlice_DatasetOne';
+dir0        = dir(strcat(baseDir,filesep,'T*'));
+dirOut      = strcat(baseDir,'_mat_Or');
+
+mkdir(dirOut)
 %%
+
 for k =1:size(dir0,1)
     disp(k)
-    dir1 = dir(strcat(dir0(k).name,'/','*.tif'));
-    dataIn1 = double(imread(strcat(dir0(k).name,'/',dir1(1).name)));
-    dataIn2 = double(imread(strcat(dir0(k).name,'/',dir1(2).name)));
+    dir1 = dir(strcat(baseDir,filesep,dir0(k).name,'/','*.tif'));
+    dataIn1 = double(imread(strcat(baseDir,filesep,dir0(k).name,'/',dir1(1).name)));
+    dataIn2 = double(imread(strcat(baseDir,filesep,dir0(k).name,'/',dir1(2).name)));
     
     dataIn(:,:,1) = (dataIn1)/max(dataIn1(:));
     dataIn(:,:,2) = (dataIn2)/max(dataIn2(:));
-    filename = strcat(dir0(k).name);
+    filename = strcat(dirOut,filesep,dir0(k).name);
     save(filename,'dataIn')
     
     
