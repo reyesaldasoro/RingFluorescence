a51f132e445dee3032e61f927ce023af693d1ce9
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-09-01 11:51:12 +0100

    Update visualise_3D.m

--- CODE/visualise_3D.m
+++ CODE/visualise_3D.m
@@ -1,4 +1,4 @@
-for k1=0:24:288
+for k1=168%:24:288
     for k2=1:12
         a(:,:,k2)   = double(imread('dataset_One.tif',k1+2*k2-1));
         b(:,:,k2)   = double(imread('dataset_One.tif',k1+2*k2));
@@ -10,8 +10,8 @@ for k1=0:24:288
     c(:,:,2)        = max(b2,[],3)/max(b2(:));
     c(1,1,3)        = 0;
 
-
-    a3=(b2>880.61972207).*a2;
+ rThres = 530;
+    a3=(b2>rThres).*a2;
     d(:,:,1)=0.4*max(a3,[],3)/max(a3(:));
     d(:,:,2)=max(b2,[],3)/max(b2(:));
     d(1,1,3)=0;
@@ -27,12 +27,17 @@ for k1=0:24:288
 
     clf
 
-    gThres =1064;
-    rThres = 530;
+    gThres =1062;
+   
     h1 = gca;
-    s1 = isosurface(b2,750);
+    % s1 is the green neutrophil
+    s1 = isosurface(b2,558);
+    % s2 is the green phagosomes
     s2 = isosurface(b2,gThres);
+    % s3 is red outside the neutrophil
     s3 = isosurface(a2,1100);
+    % s4 is red inside the neutrophil, there are some problems with the
+    % boundaries
     s4 = isosurface(a3,rThres);
 
     p1 = patch(s1);
@@ -73,6 +78,7 @@ for k1=0:24:288
 
     h1.XLim = [93 120];
     h1.YLim = [77 98];
+    h1.ZLim = [1 12];
     %h1.YLim = [80 100];
     %h1.XLim = [90 110];
     %h1.ZLim = [4 11];
@@ -85,6 +91,7 @@ for k1=0:24:288
     p2.EdgeAlpha = 0.8;
 
     filename = strcat('DataSetOne_z_t=',num2str(time),'_r=',num2str(rThres),'_g=',num2str(gThres),'.png');
-    print('-dpng','-r100',filename)
+    %print('-dpng','-r100',filename)
     pause(0.5)
+    %axis tight
 end
