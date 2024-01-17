9352e7de71b6337e501c247437592f2a17503f3f
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2024-01-17 17:15:08 +0000

    Update mergeTwoDatasets.m

--- CODE/mergeTwoDatasets.m
+++ CODE/mergeTwoDatasets.m
@@ -13,3 +13,16 @@ for counterF = 1:numFiles0
     saveName = strcat(baseDir,'Dataset_Two_Whole\',currFile(1:12),num2str((numFiles1/2)+str2num(currFile(23:26))),currFile(27:end)  );
     imwrite( currImage,saveName)
 end
+
+%% add "t" into the names of half the files
+
+baseDir     = 'C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\RingFluorescence\';
+dir0        = dir(strcat(baseDir,'Dataset_Two_Whole\*.tif'));
+%%
+% counterF = 3792;
+for counterF = 1:3791
+    disp(counterF)
+    currFileN = dir0(counterF).name;
+    newFileN = strcat(currFileN(1:12),'t',currFileN(13:end));
+    movefile (strcat(baseDir,'Dataset_Two_Whole\',currFileN), strcat(baseDir,'Dataset_Two_Whole\',newFileN))
+end
